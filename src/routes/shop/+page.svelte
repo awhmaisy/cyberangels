286667ab0a71292e43455089d8273198bc36b3ae
mei higashi <mei@meihigashi.com>
2024-08-05 22:50:27 -0700

    owo

--- src/routes/shop/+page.svelte
+++ src/routes/shop/+page.svelte
@@ -1,3 +1,15 @@
 <p>
-    drain it, sooner than later
+    call me a fancy accountant
+</p>
+<p>
+    egirl entrepreneur
+</p>
+<p>
+    sobriety virginity dignity
+</p>
+<p>
+    central incel alliance
+</p>
+<p>
+    retired asshole
 </p>
